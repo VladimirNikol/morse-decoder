07023a7df1821fd8a8919e2363af5959df6be4f6
Vladimir <vovchik.niko@gmail.com>
2022-07-30 12:56:40 +0300

    test

--- src/index.js
+++ src/index.js
@@ -35,12 +35,28 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    ' ': ' ',
 };
 
 function decode(expr) {
-    // write your solution here
+    const arrLetters = [];
+    let result = '';
+    for (i = 0; i < (expr.length / 10); i++) {
+        let letter = i * 10;
+        arrLetters.push(+expr.slice(letter, letter + 10));
+    }
+    let arrDots = arrLetters.map(n => String(n).replace(/10/g, "."));
+    let arrDash = arrDots.map(n => String(n).replace(/11/g, "-"));
+    let arrMorse = arrDash.map(n => String(n).replace(/NaN/g, " "));
+
+    for (i = 0; i < (expr.length / 10); i++) {
+
+        result += MORSE_TABLE[arrMorse[i]];
+    }
+    return result
 }
 
 module.exports = {
     decode
 }
+
